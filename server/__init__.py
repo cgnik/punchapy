1912495efac03a88ef529bf4d8973d745917075e
cgnik <cking@enmobilellc.com>
2017-12-03 12:38:44 -0500

    json encoding for mungo objects

--- server/__init__.py
+++ server/__init__.py
@@ -1,1 +1,10 @@
-import main
+import json
+from bson import ObjectId
+
+from . import main
+
+
+class MongoJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId): return str(o)
+        return json.JSONEncoder.default(self, o)
--- server/main.py
+++ server/main.py
@@ -1,14 +1,11 @@
 from os import environ
 from flask import Flask, send_from_directory, request, jsonify
 from flask_pymongo import PyMongo
-import json
-
-SERVER_PORT = environ.get('SERVER_PORT') or '8080'
-MONGO_HOST = environ.get('MONGO_HOST') or "localhost:27017"
 
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'punch'
-app.config['MONGO_URI'] = "mongodb://{}/{}".format(MONGO_HOST, app.config['MONGO_DBNAME'])
+app.config['MONGO_URI'] = "mongodb://{}/punch".format(environ.get('MONGO_HOST') or "localhost:27017")
+app.json_encoder = MongoJSONEncoder
 
 mongo = PyMongo(app)
 
@@ -20,13 +17,13 @@ def root():
 
 @app.route('/punch', methods=['GET'])
 def punch_list():
-    return jsonify([{"timestamp": p["timestamp"], "_id": str(p["_id"])} for p in mongo.db.punch.find()])
+    return jsonify([{"_id": str(p["_id"]), "timestamp": p["timestamp"]} for p in mongo.db.punch.find()])
 
 
 @app.route('/punch/<pid>', methods=['GET'])
 def punch_get(pid):
-    derp= mongo.db.punch.find({'_id': pid})
-    return jsonify([{"id" : d['_id']} for d in derp])
+    derp = mongo.db.punch.find({'_id': pid})
+    return jsonify([{"id": str(d['_id']), "timestamp": d["timestamp"]} for d in derp])
 
 
 @app.route('/punch', methods=['POST'])
@@ -35,4 +32,4 @@ def punch_save():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', int(SERVER_PORT))
+    app.run('0.0.0.0', int(environ.get('SERVER_PORT') or '8080'))
